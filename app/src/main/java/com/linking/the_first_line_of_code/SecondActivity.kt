89a34010abbb39af0db461625fa7ff01750d0ec3
Lin <JBLong@Yes.com>
2022-01-20 15:55:30 +0800

    Activity About

--- app/src/main/java/com/linking/the_first_line_of_code/SecondActivity.kt
+++ app/src/main/java/com/linking/the_first_line_of_code/SecondActivity.kt
@@ -3,7 +3,7 @@ package com.linking.the_first_line_of_code
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
