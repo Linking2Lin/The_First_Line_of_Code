2f4e5d76c512a346f92d48b7abd468ab8d8ff719
Lin <JBLong@Yes.com>
2022-01-21 18:06:58 +0800

    Android Bases Add

--- app/src/main/java/com/linking/the_first_line_of_code/SecondActivity.kt
+++ app/src/main/java/com/linking/the_first_line_of_code/SecondActivity.kt
@@ -2,10 +2,12 @@ package com.linking.the_first_line_of_code
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.StringBuilder
 
 class SecondActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        //将Intent内的数据取出到列表中
     }
 }
---
+++ app/src/main/java/com/linking/the_first_line_of_code/Temp.java
@@ -0,0 +1,5 @@
+package com.linking.the_first_line_of_code;
+
+public class Temp {
+
+}
