d79df6f2d5d0e02bcec50d695894f8d38b491123
Lin <JBLong@Yes.com>
2022-03-02 08:58:53 +0800

    C6 高阶函数

--- broadcasttest/src/main/java/com/example/broadcasttest/off/line/BaseActivity.kt
+++ broadcasttest/src/main/java/com/example/broadcasttest/off/line/BaseActivity.kt
@@ -8,6 +8,9 @@ import android.os.PersistableBundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * 所有Activity的基类，实现弹窗
+ */
 open class BaseActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
@@ -19,6 +22,9 @@ open class BaseActivity :AppCompatActivity(){
         ActivityCollector.removeActivity(this)
     }
 
+    /**
+     * 定义一个接收器，在接收到下线的广播后弹窗并销毁Activity
+     */
     inner class offlineReceiver:BroadcastReceiver(){
         override fun onReceive(context: Context?, intent: Intent?) {
             if (context != null) {
@@ -30,7 +36,6 @@ open class BaseActivity :AppCompatActivity(){
                         ActivityCollector.finishAll()//销毁所有
                         val i = Intent(context,LoginActivity::class.java)//构建启动登录界面的意图
                         context.startActivity(i)//启动登录界面
-
                     }
                 }
             }
