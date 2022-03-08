72204def1601be1d06b53e2255b517a31e787dce
Lin <JBLong@Yes.com>
2022-03-08 10:57:47 +0800

    Linking to Lin

--- retrofittest/src/main/java/com/example/retrofittest/AppService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.retrofittest
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface AppService {
-    @GET("get_data.json")//表示调用该方法会发起一条请求，请求地址为注解中的参数（相对路径）
-    fun getAppData():Call<List<App>> //返回值声明为Retrofit内置的Call类型，并通过泛型来指定服务器返回数据应该转换为什么对象
-}
