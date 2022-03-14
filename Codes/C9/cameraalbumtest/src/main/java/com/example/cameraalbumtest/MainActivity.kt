a5b91297c91fea011be8257cbd601334566dc267
Lin <JBLong@Yes.com>
2022-03-14 18:00:18 +0800

    startActivityForResult 替代

---
+++ Codes/C9/cameraalbumtest/src/main/java/com/example/cameraalbumtest/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.cameraalbumtest
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import com.example.cameraalbumtest.databinding.ActivityMainBinding
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val takePhoto = 1
+    lateinit var imageUri:Uri
+    lateinit var outputImage:File
+
+    private val myActivityLauncher = registerForActivityResult(Contract()){
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            //创建File对象，用来储存拍照后的图片
+            outputImage = File(externalCacheDir,"output_image.jpg")
+            if (outputImage.exists()){
+                outputImage.delete()
+            }
+            outputImage.createNewFile()
+            imageUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                FileProvider.getUriForFile(this,"com.example.cameraalbumtest.fileprovider",outputImage)
+            }else{
+                Uri.fromFile(outputImage)
+            }
+            //启动相机
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
+            startActivityForResult(intent,takePhoto)
+        }
+
+
+    }
+}
