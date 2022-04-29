a770b16dc247c84f7ad84a0874315422ecca4057
Lin <Lin@yes.com>
2022-04-29 13:43:21 +0800

    Linking 2 Lin

--- Codes/C15/sunnyweatherdemo/src/main/java/com/lins/sunnyweatherdemo/logic/data/layer/entry/RespositoryCaiYun.kt
+++ Codes/C15/sunnyweatherdemo/src/main/java/com/lins/sunnyweatherdemo/logic/data/layer/entry/RespositoryCaiYun.kt
@@ -6,6 +6,15 @@ import com.lins.sunnyweatherdemo.logic.network.caiyun.SunnyWeatherNetworkCaiYun
 import com.lins.sunnyweatherdemo.logic.network.hefeng.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
 
+/**
+ * 
+ * @author Lin
+ * @date 2022/4/28
+ * ----------------------------------------------------
+ * Tell me God,are you punishing me?
+ * Is this the price I'm paying for my past mistakes? *
+ * ----------------------------------------------------
+ */
 object RespositoryCaiYun {
                                      //自动构建并返回一个livedata对象，在代码块中提供一个挂起函数的上下文
     fun searchPlaces(query:String) = liveData(Dispatchers.IO){
--- Codes/C15/sunnyweatherdemo/src/main/java/com/lins/sunnyweatherdemo/ui/place/caiyun/PlaceCaiYunViewModel.kt
+++ Codes/C15/sunnyweatherdemo/src/main/java/com/lins/sunnyweatherdemo/ui/place/caiyun/PlaceCaiYunViewModel.kt
@@ -6,7 +6,17 @@ import androidx.lifecycle.ViewModel
 import com.lins.sunnyweatherdemo.logic.data.layer.entry.RespositoryCaiYun
 import com.lins.sunnyweatherdemo.logic.model.caiyun.Place
 
-class PlaceViewModelCaiYun:ViewModel() {
+/**
+ *
+ * @author Lin
+ * @date 2022/4/29
+ * ----------------------------------------------------
+ * Tell me God,are you punishing me?
+ * Is this the price I'm paying for my past mistakes? *
+ * ----------------------------------------------------
+ */
+class PlaceCaiYunViewModel:ViewModel() {
+
     private val searchLiveData = MutableLiveData<String>()
 
     val placeList = ArrayList<Place>()
