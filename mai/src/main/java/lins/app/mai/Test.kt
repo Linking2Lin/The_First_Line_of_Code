b522ed471c1e9f5b5b1d80c8790895e43f288d73
Lin <linking2lin@outlook.com>
2023-06-20 15:28:26 +0800

    update kotlin to 1.9.0 : fix compose

--- mai/src/main/java/lins/app/mai/Test.kt
+++ mai/src/main/java/lins/app/mai/Test.kt
@@ -2,12 +2,20 @@ package lins.app.mai
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 suspend fun a() {
     withContext(Dispatchers.IO) {
-        launch {
-
+        println("HHHHHH")
+        for (i in 0 ..< 10) {
+            println(i)
         }
+    }
+}
+
+fun main() {
+    runBlocking {
+        a()
     }
 }
